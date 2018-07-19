38a4ccd683e13a0db8e3fefcccd315f68c137578
Cristian  Keisz <cristian.keisz@universalalloy.com>
2018-07-19 08:01:51 +0300

    Add project files.

---
+++ FirstNodejsWebApp/server.js
@@ -0,0 +1,26 @@
+
+
+'use strict';
+var http = require('http');
+var fs = require('fs');
+var port = process.env.PORT || 1337;
+
+function send404Response(response) {
+    response.writeHead(404, { "Content-Type": "text/plain" });
+    response.write("Error 404 : File not found");
+    response.end();
+}
+
+function OnRequest(request, response) {
+    if (request.method == 'GET' && request.url == '/')
+    {
+        request.writeHead(200, { "Content-Type": "text/html" });
+        fs.createRedStream("./index.html").pipe(response);
+    } else
+    {
+        send404Response(response);
+    }
+}
+
+
+http.createServer(OnRequest).listen(port);
